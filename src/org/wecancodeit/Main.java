a54a5e7ededf49e1b2d39b569cbc569a9d7bb75e
fpemba <fatuma.pemba@yahoo.com>
2020-11-14 21:18:54 -0500

    added printGameBoard method

---
+++ src/org/wecancodeit/Main.java
@@ -0,0 +1,27 @@
+package org.wecancodeit;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}};
+
+
+        printGameBoard(gameBoard);
+
+    }
+
+    public static void printGameBoard(char[][] gameBoard) {
+
+        for(char[] row : gameBoard){
+            for(char c : row){
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+}
