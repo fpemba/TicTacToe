5a4eb4f2189d48284ef05b6a64cb9bec7d4bbd3b
fpemba <fatuma.pemba@yahoo.com>
2020-11-14 22:09:35 -0500

    added checkWinner method

--- src/org/wecancodeit/Main.java
+++ src/org/wecancodeit/Main.java
@@ -1,8 +1,11 @@
 package org.wecancodeit;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+
+    static ArrayList<Integer>playerPositions = new ArrayList<>();
+    static ArrayList<Integer>cpuPositions = new ArrayList<>();
 
     public static void main(String[] args) {
 
@@ -12,16 +15,32 @@ public class Main {
                 {'-', '+', '-', '+', '-'},
                 {' ', '|', ' ', '|', ' '}};
 
-
         printGameBoard(gameBoard);
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter your placement (1-9): ");
-        int position = scan.nextInt();
+        while(true) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter your placement (1-9): ");
+            int playerPos = scan.nextInt();
+            while(playerPositions.contains(playerPos) || cpuPositions.contains(playerPos)){
+                System.out.println("Position taken! Enter a correct Position");
+                playerPos = scan.nextInt();
+            }
 
-        System.out.println(position);
+            placePiece(gameBoard, playerPos, "player");
 
+            Random rand = new Random();
+            int cpuPos = rand.nextInt(9) + 1;
+            while(playerPositions.contains(cpuPos) || cpuPositions.contains(cpuPos)){
+                cpuPos = scan.nextInt();
+            }
 
+            placePiece(gameBoard, cpuPos, "cpu");
+
+            printGameBoard(gameBoard);
+
+            String result = checkWinner();
+            System.out.println(result);
+        }
     }
 
     public static void printGameBoard(char[][] gameBoard) {
@@ -36,10 +55,12 @@ public class Main {
 
     public static void placePiece(char[][] gameBoard, int position, String user) {
         char symbol = ' ';
-        if(user.equals("Player")){
+        if(user.equals("player")){
             symbol = 'X';
+            playerPositions.add(position);
         } else if(user.equals("cpu")){
             symbol = 'O';
+            cpuPositions.add(position);
         }
         switch (position) {
             case 1:
@@ -72,9 +93,40 @@ public class Main {
             default:
                 break;
         }
+    }
 
-        printGameBoard(gameBoard);
+    public static String checkWinner(){
 
+        List topRow = Arrays.asList(1,2,3);
+        List midRow = Arrays.asList(4,5,6);
+        List botRow = Arrays.asList(7,8,9);
+        List leftCol = Arrays.asList(1,4,7);
+        List midCol = Arrays.asList(2,5,8);
+        List rightCol = Arrays.asList(3,6,9);
+        List cross1 = Arrays.asList(1,5,9);
+        List cross2 = Arrays.asList(7,5,3);
+
+        List<List> winningConditions = new ArrayList<>();
+
+        winningConditions.add(topRow);
+        winningConditions.add(botRow);
+        winningConditions.add(midRow);
+        winningConditions.add(leftCol);
+        winningConditions.add(rightCol);
+        winningConditions.add(midCol);
+        winningConditions.add(cross1);
+        winningConditions.add(cross2);
+
+        for(List list : winningConditions){
+            if(playerPositions.contains(list)){
+                return "Congratulations! You wont!";
+            } else if(cpuPositions.contains(list)){
+                return "CPU wins! sorry!";
+            } else if(playerPositions.size() + cpuPositions.size() == 9){
+                return "CAT!";
+            }
+        }
+        return "";
     }
 
 }
